d939c2424767432c6d2beb77bc62b83161c4b6c6
RAJVEER31 <88730146+RAJVEER31@users.noreply.github.com>
2022-03-27 21:59:21 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,4 @@
-gesture_1 = ""
-gesture_2 = ""
+prediction = "";
 
 Webcam.set({
     width: 350,
@@ -7,29 +6,73 @@ Webcam.set({
     image_format: 'png',
     png_quality: 90
 });
+
 camera = document.getElementById("camera");
-Webcam.attach("#camera");
-function take_snapshot() { 
-    Webcam.snap(function (data_uri) { document.getElementById("result").innerHTML = '<img id="captured_image" src="' + data_uri + '"/>'; }); 
+
+Webcam.attach('#camera');
+
+function take_snapshot() {
+    Webcam.snap(function (data_uri){
+        document.getElementById("result").innerHTML = '<img id="image_captured" src="'+data_uri+'"/>';
+    });
 }
-console.log(ml5.verson);
-clasifier = ml5.imageClasifier("https://teachablemachine.withgoogle.com/models/5mxzZHkpx/model.json",modelloded);
-function modelloded() {
-    console.log("modelloded");
+
+console.log("ml5 version:",ml5.version);
+
+classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/Qorn6llzx/model.json',modelLoaded);
+
+function modelLoaded() {
+    console.log("Model Loaded Successfully!");
 }
+
 function speak() {
-    var synth = window.speachSynthesis;
-    speak1 = "prediction 1 is" + prediction_1;
-    speak2 = "prediction 2 is" + prediction_2;
-    var say = new SpeechSynthesisUtterance(speak1 + speak2);
-    synth.speak(say);
+    var synth = window.speechSynthesis;
+    var speak_data = "The Prediction Is "+prediction;
+    var utterThis = new SpeechSynthesisUtterance(speak_data);
+    synth.speak(utterThis);
 }
 
 function check() {
-    img = document.getElementById("captured_image");
-    identify.classify(img, list);
+    img = document.getElementById("image_captured");
+    classifier.classify(img, gotResults);
 }
 
-function list() {
-
+function gotResults(error, results){
+    if(error){
+        console.error(error);
+    }
+    else{
+        console.log(results);
+        document.getElementById("result_gesture_name").innerHTML = results[0].label;
+        prediction = results[0].label;
+        speak();
+        if(results[0].label == "Amazing"){
+            document.getElementById("result_emoji").innerHTML = "&#128076;";
+            document.getElementById("quote").innerHTML = "When you have a dream, you've got to grab it and never let go";
+        }
+        if(results[0].label == "Best"){
+            document.getElementById("result_emoji").innerHTML = "&#128077";
+            document.getElementById("quote").innerHTML = "The best is yet to be";
+        }
+        if(results[0].label == "Victory"){
+            document.getElementById("result_emoji").innerHTML = "&#9996;";
+            document.getElementById("quote").innerHTML = "If you believe in yourself and have dedication and pride and never quit, you'll be a winner";
+        }
+        if(results[0].label == "Unity"){
+            document.getElementById("result_emoji").innerHTML = "&#9994;";
+            document.getElementById("quote").innerHTML = "Unity to be real must stand the severest strain without breaking";
+        }
+        if(results[0].label == "Clap"){
+            document.getElementById("result_emoji").innerHTML = "&#128079;";
+            document.getElementById("quote").innerHTML = "An aged man is but a paltry thing, a tattered coat upon a stick, unless soul clap its hands and sing, and louder sing for every tatter in its mortal dress";
+        }
+        if(results[0].label == "Rock"){
+            document.getElementById("result_emoji").innerHTML = "&#129304;";
+            document.getElementById("quote").innerHTML = "Oh! You Are On A Party Enjoy It";
+        }
+        if(results[0].label == "Bye"){
+            document.getElementById("result_emoji").innerHTML = "&#128075;";
+            document.getElementById("quote").innerHTML = "Same To You Bye Bye";
+        }
+    }
 }
